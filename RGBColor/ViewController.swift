71ff04345924cd660af2efd02f8f81ccf62efadc
kitsuyy <lozovoi29111995@gmail.com>
2022-09-27 21:25:23 +0400

    Initial Commit

---
+++ RGBColor/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  RGBColor
+//
+//  Created by Kirill Lozovoi on 9/27/22.
+//
+
+import UIKit
+
+
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var VIew: UIView!
+    
+    @IBOutlet var redLB: UILabel!
+    @IBOutlet var greenLB: UILabel!
+    @IBOutlet var blueLB: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        VIew.layer.cornerRadius = 20
+        setColor()
+    }
+    
+    
+    @IBAction func sliderAction(_ sender: UISlider) {
+        setColor()
+        redLB.text = string(from: redSlider)
+        greenLB.text = string(from: greenSlider)
+        blueLB.text = string(from: blueSlider)
+    }
+    
+    private func setColor() {
+        VIew.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1)
+    }
+    
+    private func string(from slider: UISlider) -> String {
+        String(format: "%.2f", slider.value)
+    }
+}
+    
